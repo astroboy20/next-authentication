c44c4c47fe18da62734f1021350b9e34abbbe464
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-03-23 02:15:12 +0100

    animation setup

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -9,8 +9,8 @@ const Layout = ({children}:Props) => {
         <div className='m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
             <div className={styles.imgStyle}>
                 <div className={styles.cartoonImg}></div>
-                <div></div>
-                <div></div>
+                <div className={styles.cloud_one}></div>
+                <div className={styles.cloud_two}></div>
             </div>
             <div className='right flex flex-col justify-evenly bg-gray-500'>
                 <div className='text-center py-10'> 

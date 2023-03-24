22fda84edfdb48ccd642a853cd04b9ee899978e3
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-03-24 22:38:34 +0100

    update

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Layout from '@/layout/layout'
 import Link from 'next/link'
+import styles from '../styles/Form.module.css'
 const Login = () => {
   return (
     
@@ -17,12 +18,12 @@ const Login = () => {
             <p className='w-3/4 mx-auto text-gray-400'> voluptatum, ducimus quis quas numquam nam</p>
         </div>
         {/* form */}
-        <form className='flex flex-col gap-5'>
-            <div className="input-group">
-                <input type="email" name='email' placeholder='Email' />
+        <form className='flex flex-col gap-5 text-center'>
+            <div className={styles.input_group}>
+                <input className={styles.input_text} type="email" name='email' placeholder='Email' />
             </div>
-            <div className="input-group">
-                <input type="password" name='password' placeholder='Password' />
+            <div className={styles.input_group}>
+                <input className={styles.input_text} type="password" name='password' placeholder='Password' />
             </div>
             {/* login buton */}
             <div className="input-button">
